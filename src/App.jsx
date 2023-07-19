6afdaae66f38e89eb842e85f7461760b26494463
Soukayna Alala <soukayna.alala45@gmail.com>
2023-07-19 14:47:40 +0100

    added fortawesome

--- src/App.jsx
+++ src/App.jsx
@@ -5,8 +5,6 @@ function App() {
   return (
     <div>
       <Header />
-      app component
-      <Header />
       <Profile />
     </div>
   );
--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -2,11 +2,6 @@ import styles from "./Header.module.scss";
 
 export function Header() {
   const { text } = styles;
-  function getSoukaynasAge(age = 18) {
-    return age;
-  }
 
-  const soukaynaAge = getSoukaynasAge(100);
-
-  return <p className={text}>Header Component {soukaynaAge}</p>;
+  return <p className={text}>Header Component</p>;
 }

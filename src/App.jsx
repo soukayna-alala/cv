c75a52ee009b7b59ea36b98f6dfeef714f8e7199
Soukayna Alala <soukayna.alala45@gmail.com>
2023-08-02 18:56:31 +0100

    created Skills component

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import { Header } from "./components/Header/Header.jsx";
 import { Profile } from "./components/Profile/Profile.jsx";
 import styles from "./App.module.scss";
+import { Skills } from "./components/Skills/Skills";
 
 function App() {
   return (
@@ -9,6 +10,7 @@ function App() {
       <div className={styles.gridContainer}>
         <div>
           <Profile />
+          <Skills />
         </div>
       </div>
     </>
---
+++ src/components/Skills/Skills.jsx
@@ -0,0 +1,12 @@
+import styles from "./Skills.module.scss";
+
+export const Skills = () => {
+  return (
+    <div>
+      <h3>Skills</h3>
+      <p className={styles.copy}>
+        HTML5, CSS3, Media Queries, Git, SCSS, JavaScript, React, Vite
+      </p>
+    </div>
+  );
+};

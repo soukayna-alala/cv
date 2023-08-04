5cda89d5aac9d003dbc4b466022bf1ad8626eaa3
Soukayna Alala <soukayna.alala45@gmail.com>
2023-08-04 17:06:40 +0100

    created Interests component

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import styles from "./App.module.scss";
 import { Skills } from "./components/Skills/Skills";
 import { CodeSamples } from "./components/CodeSamples/CodeSamples";
 import { FindMeOnline } from "./components/FindMeOnline/FindMeOnline";
+import { Interests } from "./components/Interests/Interests";
 
 function App() {
   return (
@@ -19,6 +20,7 @@ function App() {
           <FindMeOnline />
         </div>
       </div>
+      <Interests />
     </>
   );
 }
---
+++ src/components/Interests/Interests.jsx
@@ -0,0 +1,13 @@
+import styles from "./Interests.module.scss";
+
+export const Interests = () => {
+  return (
+    <div>
+      <h3>Interests</h3>
+      <p className={styles.copy}>
+        Front end web development, learning new technologies, travelling,
+        cinema.
+      </p>
+    </div>
+  );
+};
